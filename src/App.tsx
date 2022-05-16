3bbe7a0618e48b702041e480a8f6659bb51550f8
Tima <tima52rus@gmail.com>
2022-05-16 23:35:54 +0300

    create JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { AppRootStateType } from './store';
+import { calcByAmount, decrement, increment } from './store/countReducer';
 
-function App() {
+export const App = () => {
+
+  const [value, setValue] = useState('')
+  const count = useSelector((state: AppRootStateType) => state.counter.value)
+  const dispatch = useDispatch()
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{count}</span>
+      <input type="text" 
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <div>
+        <button
+          aria-label='increment count'
+          onClick={() => dispatch(increment())}
+          disabled={count === 10}
+        >increment</button>
+        <button
+          aria-label='decrement'
+          onClick={() => dispatch(decrement())}
+          disabled={count === 0}
+        >decrement</button>
+        <button
+          aria-label='increment by value'
+          onClick={() => dispatch(calcByAmount(+value))}
+          >
+          increment By Value
+        </button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
